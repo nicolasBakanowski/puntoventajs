1f0e747eceafaed8fdac30a3c94a8c6358003ed9
Nicolas Bakanowski <nicolasbakanowski@gmail.com>
2020-05-22 13:32:17 -0300

    first commit

---
+++ categorias.js
@@ -0,0 +1,96 @@
+let myStorage = window.localStorage;
+
+
+let PVenta = { 
+    /*  creamos un espacio de nombre PVenta
+        vamos a crear un CRUD (Create, Read, Update, Delete)
+        Vamos almacenar nuestras objetos en Arrays y respaldarlos con localStorage
+        recordar que el id de cada Objeto debe ser unico en su Lista
+    */
+
+    _usuarios:[], 
+//  Usuario(id,usuario,clave)
+                  //  Ejemplo  {id:0,usuario:'admin',clave:'root'}
+    _categorias:[], // Categoria (id, nombre , idPadre , detalle, porcgancia) 
+                    // Ejemplo Categoria {id:0,nombre:'General',idPadre:0,detalle:'Todas las Categorias',porcganancia:35.5}
+    _productos:[], // Producto(id,codigo,foto,nombre,idCategoria,precioc,preciov,conteo:[KILO | UNIDAD])
+    _facturas:[],
+    _detFacturas:[],
+
+    
+    // Metodos de nuestro Punto de Venta
+    // Recordar solo agregar los metodos de la Api.
+
+    getcategoria:function(idcategoria){
+       let usuario=null;
+       PVenta._categorias.map((u)=> {if (u.id == idcategoria) return categoria=u});
+       return categoria;
+    },
+    getcategorias(){
+        return PVenta._categorias
+    },
+    
+
+    deleteUsuario(idcategoria){
+        let indice=-1; // no hemos encontrado nada aun
+        
+        for (let i=0; i<PVenta._categorias.length;i++){
+            if (PVenta._usuarios[i].id == idcategoria){ 
+                indice =i;
+            }
+        }    
+    
+        if (indice>=0){ //hemos localizado el objeto persona
+                PVenta._categorias.splice(indice,1);
+                this.saveData();
+        }
+     },
+    updateUsuario(usuario){
+        let indice=-1; // no hemos encontrado nada aun
+
+        for (let i=0; i<PVenta._usuarios.length;i++){
+            if (PVenta._usuarios[i].id == usuario.id) 
+                indice =i;
+        }
+        if (indice>=0){ //hemos localizado el objeto persona
+            PVenta._usuarios[indice]=usuario;
+            this.saveData();
+        }
+    },
+
+    addcategoria(categoria){
+       PVenta._categorias.push(categoria);
+       this.saveData();
+
+    },
+    saveData(){
+        let data={
+            usuarios:this._usuarios,
+            categorias: this._categorias,
+            productos: this._productos,
+            facturas: this._facturas,
+            detFacturas: this._detFacturas
+        }
+        myStorage.setItem('datajson',JSON.stringify(data));
+        console.log('OK');
+    },
+
+    init:function(){
+        //cargamos todos nuestros array del localStorage
+    let data = myStorage.getItem('datajson');
+    if (data){
+        data = JSON.parse(data);
+        this._usuarios = data.usuarios;
+        this._categorias = data.categorias;
+        this._productos = data.productos;
+        this._facturas = data.facturas;
+        this._detFacturas = data.detFacturas;       
+    }
+    
+        
+    }
+
+}
+
+PVenta.init();// metodo obligatorio primero que todos para cargar datos desde el localStorage
+PVenta.getUsuarios();
---
+++ pventa.js
@@ -0,0 +1,107 @@
+let myStorage = window.localStorage;
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })
+
+let PVenta = { 
+    /*  creamos un espacio de nombre PVenta
+        vamos a crear un CRUD (Create, Read, Update, Delete)
+        Vamos almacenar nuestras objetos en Arrays y respaldarlos con localStorage
+        recordar que el id de cada Objeto debe ser unico en su Lista
+    */
+
+    _usuarios:[], 
+//  Usuario(id,usuario,clave)
+                  //  Ejemplo  {id:0,usuario:'admin',clave:'root'}
+    _categorias:[], // Categoria (id, nombre , idPadre , detalle, porcgancia) 
+                    // Ejemplo Categoria {id:0,nombre:'General',idPadre:0,detalle:'Todas las Categorias',porcganancia:35.5}
+    _productos:[], // Producto(id,codigo,foto,nombre,idCategoria,precioc,preciov,conteo:[KILO | UNIDAD])
+    _facturas:[],
+    _detFacturas:[],
+
+    
+    // Metodos de nuestro Punto de Venta
+    // Recordar solo agregar los metodos de la Api.
+
+    getUsuario:function(idUsuario){
+       let usuario=null;
+       PVenta._usuarios.map((u)=> {if (u.id == idUsuario) return usuario=u});
+       return usuario;
+    },
+    getUsuarios(){
+        return PVenta._usuarios;
+    },
+    
+    getpositionUser(usuario){
+        let pos = null
+        foreach (PVenta._usuarios in objeto);{
+            pos += 1
+            if (objeto.id == usuario){
+                return pos
+            };
+        };
+    },
+
+    deleteUsuario(idUsuario){
+        let indice=-1; // no hemos encontrado nada aun
+        
+        for (let i=0; i<PVenta._usuarios.length;i++){
+            if (PVenta._usuarios[i].id == idUsuario){ 
+                indice =i;
+            }
+        }    
+    
+        if (indice>=0){ //hemos localizado el objeto persona
+                PVenta._usuarios.splice(indice,1);
+                this.saveData();
+        }
+     },
+    updateUsuario(usuario){
+        let indice=-1; // no hemos encontrado nada aun
+
+        for (let i=0; i<PVenta._usuarios.length;i++){
+            if (PVenta._usuarios[i].id == usuario.id) 
+                indice =i;
+        }
+        if (indice>=0){ //hemos localizado el objeto persona
+            PVenta._usuarios[indice]=usuario;
+            this.saveData();
+        }
+    },
+
+    addUsuario(usuario){
+       PVenta._usuarios.push(usuario);
+       this.saveData();
+
+    },
+    saveData(){
+        let data={
+            usuarios:this._usuarios,
+            categorias: this._categorias,
+            productos: this._productos,
+            facturas: this._facturas,
+            detFacturas: this._detFacturas
+        }
+        myStorage.setItem('datajson',JSON.stringify(data));
+        console.log('OK');
+    },
+
+    init:function(){
+        //cargamos todos nuestros array del localStorage
+    let data = myStorage.getItem('datajson');
+    if (data){
+        data = JSON.parse(data);
+        this._usuarios = data.usuarios;
+        this._categorias = data.categorias;
+        this._productos = data.productos;
+        this._facturas = data.facturas;
+        this._detFacturas = data.detFacturas;       
+    }
+    
+        
+    }
+
+}
+
+PVenta.init();// metodo obligatorio primero que todos para cargar datos desde el localStorage
+PVenta.getUsuarios();
